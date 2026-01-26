76fb84fc7b3b7163c7224ac0f70edb3de3f4d008
mikolajm190 <mikolaj.mo158+github@gmail.com>
2026-01-26 10:40:48 +0100

    flake: use host-specific config paths

--- flake.nix
+++ flake.nix
@@ -19,13 +19,13 @@
     nixosConfigurations = {
       kdeciak = nixpkgs.lib.nixosSystem {
         modules = [
-          ./configuration.nix
+          ./hosts/kdeciak/configuration.nix
 
           home-manager.nixosModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
-            home-manager.users.mikolajm = import ./home.nix;
+            home-manager.users.mikolajm = import ./hosts/kdeciak/home.nix;
           }
 
           nvf.nixosModules.default
