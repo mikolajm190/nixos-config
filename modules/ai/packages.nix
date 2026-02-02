a2ca1aa7decc8e159f9ccc29feefd98b3eac5510
mikolajm190 <mikolaj.mo158+github@gmail.com>
2026-02-02 17:34:07 +0100

    feat: add codex, llama.cpp and cross vendor ollama setup

--- modules/ai/packages.nix
+++ modules/ai/packages.nix
@@ -1,13 +1,32 @@
-{ pkgs, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    opencode
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+let
+  cfg = config.ai;
+  ollamaPkgs =
+    lib.optional config.gpu.nvidia.enable pkgs.ollama-cuda
+    ++ lib.optional config.gpu.amd.enable pkgs.ollama-rocm
+    ++ lib.optional config.gpu.intel.enable pkgs.ollama-vulkan;
+  llamacppPkgs = lib.optional cfg.llama-cpp.enable pkgs.llama-cpp;
+  aiPkgs =
+    ollamaPkgs
+    ++ llamacppPkgs
+    ++ lib.optional cfg.opencode.enable pkgs.opencode
+    ++ lib.optional cfg.codex.enable pkgs.codex;
+in
+{
+  options.ai = {
+    enable = lib.mkEnableOption "enable ai module";
+    ollama.enable = lib.mkEnableOption "enable ollama";
+    llama-cpp.enable = lib.mkEnableOption "enable llama.cpp";
+    opencode.enable = lib.mkEnableOption "enable opencode";
+    codex.enable = lib.mkEnableOption "enable openai codex";
+  };
 
-    (ollama.override {
-      acceleration = "cuda";
-    })
-    # (llama-cpp.override {
-    #   cudaSupport = true;
-    # })
-  ];
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = aiPkgs;
+  };
 }
