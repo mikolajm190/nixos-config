8bf5de5d2733536180a47577924aa4d484a28410
mikolajm190 <mikolaj.mo158+github@gmail.com>
2026-07-25 16:53:49 +0200

    shell: add nix system aliases

--- hosts/nikslap/home.nix
+++ hosts/nikslap/home.nix
@@ -14,6 +14,12 @@ in
   home.username = username;
   home.homeDirectory = "/home/${username}";
 
+  home.shellAliases = {
+    ll = "ls -la";
+    update = "nix flake update && sudo nixos-rebuild switch --flake .#nikslap";
+    rebuild = "sudo nixos-rebuild switch --flake .#nikslap";
+  };
+
   home-desktop.hyprland.enable = true;
 
   home.pointerCursor = {
