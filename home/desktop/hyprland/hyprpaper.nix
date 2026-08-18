db62b4f318baaa0788efea6fefd874b9e0820d3b
mikolajm190 <mikolaj.mo158+github@gmail.com>
2026-08-18 21:42:48 +0200

    hyprpaper: add new wallpaper setup

--- home/desktop/hyprland/hyprpaper.nix
+++ home/desktop/hyprland/hyprpaper.nix
@@ -1,28 +1,31 @@
 {
-  pkgs,
   lib,
   config,
   ...
 }:
 let
   cfg = config.home-desktop.hyprland;
-  wallpaper = pkgs.runCommand "hyprland-wallpaper.png" { nativeBuildInputs = [ pkgs.imagemagick ]; } ''
-    magick -size 1920x1080 canvas:'#1b1d26' \
-      -fill '#7ebae4' -draw 'polygon 757,401 809,371 1048,784 996,815' \
-      -fill '#5277c3' -draw 'polygon 915,371 967,401 728,815 676,784' \
-      -fill '#7ebae4' -draw 'polygon 1036,512 1036,572 558,572 558,512' \
-      -fill '#5277c3' -draw 'polygon 1163,678 1111,709 872,294 924,265' \
-      -fill '#7ebae4' -draw 'polygon 1005,709 953,678 1192,265 1244,294' \
-      -fill '#5277c3' -draw 'polygon 884,567 884,506 1362,506 1362,567' \
-      "$out"
-  '';
+  path = "/home/lordjim/Pictures/wallpapers/nix-wallpaper-dracula.png";
 in
 {
   config = lib.mkIf cfg.enable {
-    xdg.configFile."hypr/hyprpaper.conf".text = ''
-      preload = ${wallpaper}
-      wallpaper = ,${wallpaper}
-      splash = false
-    '';
+    services.hyprpaper = {
+      enable = true;
+
+      settings = {
+        ipc = true;
+        splash = false;
+
+        preload = [ path ];
+
+        wallpaper = [
+          {
+            monitor = "";
+            path = path;
+            fit_mode = "cover";
+          }
+        ];
+      };
+    };
   };
 }
